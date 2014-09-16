f77c44848fde2d85721b94ddee69fe033b5a7cae
Dmitrii Chudinov <piroru@gmail.com>
2014-09-16 15:42:29 +0700

    delete all files

--- README.md
+++
@@ -1,6 +0,0 @@
-news-project
-============
-
-News project 08-09 2014 
-
-Dmitrii Chudinov
--- design/footer.php
+++
@@ -1,4 +0,0 @@
-</div>
-        <div class="footer"> &COPY; Php online <?=date('Y')?> </div>
-    </body>
-</html>
