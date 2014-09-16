5eb24f27f6fcd526555d70ca8e50949015290c2c
Dmitrii Chudinov <piroru@gmail.com>
2014-09-16 16:05:14 +0700

    delete try 2

--- README.md
+++
@@ -1,4 +0,0 @@
-news-project
-============
-
-News project 08-09 2014
--- logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-require('include.php');
-$auth->logout();
-header('Location: ' . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PROJECT_PATH)  );
