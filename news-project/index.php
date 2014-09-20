23baf208bf560d3d03e5c00d12f8556d3bee5fd6
Lev Sizov <sizov.rob2011@yandex.ru>
2014-09-20 20:39:00 +0700

    News class fixes

--- news-project/index.php
+++ news-project/index.php
@@ -18,8 +18,7 @@
         echo "Страница не найдена";
     } else {
         $news = array_slice($news, ($page-1)*$news_per_page, $news_per_page);
-        for($id = 0; $id < count($news); $id++) {
-            $news_element = $news[$id];
+        foreach($news as $id => $news_element) {
             NewsItemWriter::writeShortNews($news_element, $id);
         }
     }
