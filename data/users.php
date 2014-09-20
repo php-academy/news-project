7390236fae665e04805247a261063395bc993fe6
natali <natalix83@mail.ru>
2014-09-20 15:29:32 +0700

    Get users from BD

--- data/users.php
+++ data/users.php
@@ -15,12 +15,24 @@ $arUsers = array(
 );
 
 
+$stmt = Database :: prepare ( "select u.user_id,u.login,u.password,u.salt,r.name as role, up.name, up.age, up.avatar"
+        . " from users u inner join user_role ur on u.user_id=ur.user_id inner join roles r on ur.role_id=r.role_id"
+        . " inner join user_profile up on u.user_id=up.user_id;" ) ;
+$stmt -> execute ( ) ;
+
+
+
+$result = $sth->fetchAll(PDO::FETCH_CLASS, "User");
+
+
 
 //select * from users inner join user_role on users.user_id=user_role.user_id inner join roles on user_role.role_id=roles.role_id;
-$data = array();
+/*$data = array();
 
 foreach( $arUsers as $userId => $arUser ){
     $data[] = new User($userId, $arUser['login'], $arUser['password'], $arUser['salt'], $arUser['role']);
-}
+}*/
 
-return $data;
+$stmt -> closeCursor ( ) ;
+
+return $result;
