d13a11d3093b66e3ec1f3b816bd9dd077d65a1b0
natali <natalix83@mail.ru>
2014-09-20 16:54:21 +0700

    Добавляем регистрацию

--- registration.php
+++ registration.php
@@ -1,5 +1,7 @@
 <?php
 require('include.php');
+require(ROOT_PROJECT_PATH . '/design/header.php');
+
 if(isset($_POST["login"]) && isset($_POST["password"]) && isset($_POST["repeat"]) 
         && isset($_POST["name"]) && isset($_POST["age"]) ){
     
@@ -10,6 +12,7 @@ if(isset($_POST["login"]) && isset($_POST["password"]) && isset($_POST["repeat"]
         
         $_SESSION["registr_result"]=$arResult["message"];
          header('Location: ' . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PROJECT_PATH)  );
+        unset($_SESSION["registr_result"]);
     }
 }
 
@@ -22,15 +25,18 @@ if(isset($_SESSION["registr_result"])){
 ?>
 
 <form method="POST" action="registration.php">
-    <label>Логин
-    <input type="text" name="login"/><label>
-    <label>Пароль
-      <input type="password" name="password"/><label>   
-          <label>Повторите пароль
-      <input type="password" name="repeat"/><label>  
-          <label>Имя
-    <input type="text" name="name"/><label>
-        <label>Возраст
-    <input type="text" name="age"/><label>
+    <p><label>Логин
+    <input type="text" name="login"/><label></p>
+     <p><label>Пароль
+      <input type="password" name="password"/><label></p>   
+           <p><label>Повторите пароль
+      <input type="password" name="repeat"/><label><p>  
+           <p><label>Имя
+    <input type="text" name="name"/><label></p>
+         <p><label>Возраст
+    <input type="text" name="age"/><label><p>
+         <p><input type="submit" value="Ok"></p>
     
 </form>
+<?php
+require( ROOT_PROJECT_PATH . '/design/footer.php');
