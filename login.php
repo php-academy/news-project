7bbe3376b9f8936c768ab0345d94fb8d629a71b2
natali <natalix83@mail.ru>
2014-09-23 20:35:58 +0700

    ajax login

--- login.php
+++ login.php
@@ -1,5 +1,15 @@
 <?php
 require('include.php');
+$rememberMe = (isset($_POST['rememberMe']) && $_POST['rememberMe'] == 'on') ? true : false;
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$password = isset($_POST['password'])  ? $_POST['password'] : '';
+
+$result = $auth->login($login,  $password, $rememberMe);
+echo json_encode($result);
+
+
+
+/* old - require('include.php');
 
 if( 
     isset($_POST['login']) && 
@@ -10,4 +20,4 @@ if(
     header('Location: ' . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PROJECT_PATH)  );
 } else {
     header('Location: ' . PROJECT_PATH );
-}
+}*/
