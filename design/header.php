d8110f73b3535a59a53c19100ea4a79a9e480118
Roman <hurman@ngs.ru>
2014-09-25 16:24:50 +0700

    fix: ajax

--- design/header.php
+++ design/header.php
@@ -15,17 +15,33 @@
                             rememberMe: $("#auth_remember").val()
                         }).done(function(response) {
                          response = JSON.parse(response);
-                        if(response && response.result) {
                          
-                           $("#auth_area").html('<div id="auth_data"><i>Вы вошли как ' + response.result.login + '</i> <a href="<?=PROJECT_PATH?>/logout.php">Выход</a></p>');
+                        if(response && response.result) {
+                           $("#auth_form").hide();
+                           $("#auth_area").html('<div id="auth_data"><i>Вы вошли как ' + response.data.login + '</i> <a id="auth_logout" href="" >Выход</a></div>');
+                           $("#auth_ok").text(response.message);
                         } else {
                            $("#auth_error").text(response.message);
                         }
                 });
                 return false;
+             });    
+                
+            
+                $("#auth_logout").click(function() {
+                    $.post('logout.php', {}).done(function(response) {
+                         response = JSON.parse(response);
+                        
+                        if(response && response.result) {
+                        
+                        $("#auth_data").empty();
+                        $("#auth_form").show();
+                        }
+                    });
+                return false;
+                });
             });
         
-        });
         </script>
     </head>
     <body>
@@ -33,20 +49,21 @@
             <span class="title">Это новостной портал </span>    
            
         
-       <div id="auth_area" class="auth-area">
+        <div id="auth_area" class="auth-area"></div>
     
         <div id="auth_form" >
             <form>
-                <input id="auth_login" type="text" name="login" >
-                <input id="auth_password" type="password" name="password" >
-                <input id="auth_remember" type="checkbox" name="rememberMe" >
-                <input id="auth_button" type="button" value="Вход">
+                <input id="auth_login" type="text" name="login" />
+                <input id="auth_password" type="password" name="password" />
+                <input id="auth_remember" type="checkbox" name="rememberMe" />
+                <input id="auth_button" type="button" value="Вход" />
             </form>
             <div class="reg_link"><a href="<?=PROJECT_PATH?>/registration.php">Зарегистрироваться</a> 
             </div>
         </div>
+          
        
-           <div id="auth_error" class="error"></div>
-       </div>
+            <div id="auth_ok" class="ok"></div>
+            <div id="auth_error" class="error"></div>
     </div>
         <div class="content">
