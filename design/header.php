0fbd08c7c19d426de8db7853f9939565af2feee7
natali <natalix83@mail.ru>
2014-09-28 15:54:50 +0600

    news load from DB

--- design/header.php
+++ design/header.php
@@ -16,13 +16,25 @@
                     }).done(function(response){
                         response = JSON.parse(response);
                         if( response && response.result ){
-                            $("#auth_area").html('<div id="auth_data"><i>' + response.data.login + '</i> <a href="<?=PROJECT_PATH?>/logout.php">выход</a></div>');
+                            $("#auth_area").html('<div id="auth_data"><i>' + response.data.login + '</i> <a id="auth_logout" href="<?=PROJECT_PATH?>/logout.php">выход</a></div>');
                         } else {
                             $("#auth_error").text(response.message);
                         }
                     });
                     return false;
                 });
+                $("#auth_logout").click(function(){
+                    $.post('/news_project/logout.php',{}).done(function(response){
+                        response = JSON.parse(response);
+                        if( response && response.result ) {
+                            $("#auth_area").html(
+                                    "<div id='auth_form'><form action='<?=PROJECT_PATH?>/login.php' method='POST' ><input id='auth_login' type='text' name='login' ><input id='auth_password' type='password' name='password' ><input id='auth_remember' type='checkbox' name='rememberMe' ><input type='submit' value='вход' id='auth_button' ></form><div class='reg_link'><a  href='<?=PROJECT_PATH?>/registration.php'>зарегистрироваться</a></div></div><div id='auth_error' class='error'></div>"
+                            );
+                        }
+                    });
+                    return false;
+                });
+
             });
         </script>
 
@@ -35,7 +47,7 @@
             <div class="login-area">
                 <?php
                 if( $user = $auth->getAuthorizedUser() ) {
-                    ?><p class="login-line"><i><?=$user->login?></i> <a href="<?=PROJECT_PATH?>/logout.php">Выход</a></p><?php
+                    ?><p class="login-line"><i><?=$user->login?></i> <a id="auth_logout" href="<?=PROJECT_PATH?>/logout.php">Выход</a></p><?php
                 } else {
                     ?>
                         <form action="<?=PROJECT_PATH?>/login.php" method="POST" >
@@ -44,7 +56,7 @@
                             <input type="checkbox" name="rememberMe" >
                             <input type="submit" value="Вход">
                         </form>
-                    <br><a href="<?=PROJECT_PATH?>/registration.php"</a>Registraciya;
+                    <br><a href="<?=PROJECT_PATH?>/registration.php"</a>Зарегистрироваться
                 <?php } ?>
                 <?php
                 if( isset($_SESSION['login_error_message']) && trim($_SESSION['login_error_message']) ){
